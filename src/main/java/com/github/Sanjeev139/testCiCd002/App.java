ef277da3298660b9ab7225dfee57d60257ed06b3
Bingyan Sahoo <bingyan.sahoo@intimetec.com>
2020-03-26 11:20:29 +0530

    changes

---
+++ src/main/java/com/github/Sanjeev139/testCiCd002/App.java
@@ -0,0 +1,13 @@
+package com.github.Sanjeev139.testCiCd002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( new Calc().getInt(2, 8));
+    }
+}
---
+++ src/main/java/com/github/Sanjeev139/testCiCd002/Calc.java
@@ -0,0 +1,13 @@
+package com.github.Sanjeev139.testCiCd002;
+
+public class Calc {
+	
+	public int getInt(int x, int y)
+	{
+		return x+y;
+	}
+	public String getString(String x, String y)
+	{
+		return x+y;
+	}
+}
